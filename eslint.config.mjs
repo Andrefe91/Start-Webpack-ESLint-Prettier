160abc94a93ee0c199081258223f75a976f9a73f
Andres Muñoz <andrefe91@gmail.com>
2024-07-08 12:24:55 -0500

    Template setup

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    "ignorePattern": "webpack.config.js",
+  },
+  pluginJs.configs.recommended,
+];
